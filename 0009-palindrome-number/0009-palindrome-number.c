48a3345657c227adce0a9ee924800adad2c47b89
Bishal Saha <saha.bishal7676@gmail.com>
2024-12-29 10:20:18 +0530

    Time: 3 ms (40.38%), Space: 8.3 MB (15.99%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.c
@@ -0,0 +1,28 @@
+#define FALSE false /*Macro defination*/
+#define TRUE true
+
+bool isPalindrome(int x) {
+    if (x < 0 || (x % 10 == 0 && x != 0)) { /*Negetive number never become Palindrome number*/
+        return FALSE;
+    }
+
+    int realnum = x;
+    int revnum = 0;
+
+    while (x > 0) {
+        int remnum = x % 10;
+
+        if (revnum > (INT_MAX - remnum) / 10) {
+            return false; // Overflow detected
+        }
+        
+        revnum = revnum * 10 + remnum;
+        x /= 10;
+    }
+
+    if (realnum == revnum) {
+        return TRUE;
+    }
+
+    return FALSE;
+}
