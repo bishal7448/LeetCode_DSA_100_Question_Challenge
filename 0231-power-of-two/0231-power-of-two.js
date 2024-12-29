a745707c49180e5d294b452aea99b0449894328a
Bishal Saha <saha.bishal7676@gmail.com>
2024-12-29 11:14:27 +0530

    Time: 2 ms (29.01%), Space: 52.1 MB (98.33%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function (n) {
+    if (n < 1) {
+        return false;
+    } else if (n === 1) {
+        return true;
+    } else {
+        while (n % 2 === 0) {
+            n = n / 2;
+        }
+        if (n === 1) {
+            return true;
+        }
+        return false;
+    }
+};
