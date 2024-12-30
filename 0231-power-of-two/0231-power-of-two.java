9523ae3aa89e294f0c38279675e6fef097b200f1
Bishal Saha <saha.bishal7676@gmail.com>
2024-12-30 09:38:03 +0530

    Time: 1 ms (42.19%), Space: 41.4 MB (6.79%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if (n < 1) {
+            return false;
+        }
+
+        // if (n == 1) {
+        //     return true;
+        // }
+
+        if (n % 2 == 0) {
+            while (n % 2 == 0) {
+                n = n / 2;
+            }
+        }
+
+        if (n == 1) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+}
