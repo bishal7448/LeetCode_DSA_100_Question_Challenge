926d0faf95d77166bc3a840917da16a21f2a9e31
Bishal Saha <saha.bishal7676@gmail.com>
2025-01-01 11:47:38 +0530

    Time: 5 ms (71.6%), Space: 44.4 MB (14.72%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,23 +1,20 @@
 class Solution {
     public boolean isPalindrome(int x) {
         
-        if(x < 0 || (x % 10 == 0 && x != 0)){
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
             return false;
         }
 
-        int realNum = x;
-        int revNum = 0;
+        int realnum = x;
+        int revnum = 0;
 
-        while(x > 0){
-
-            int remNum = x % 10;
-
-            revNum = revNum * 10 + remNum;
-
-            x/=10;
+        while (x > 0) {
+            int remnum = x % 10;
+            revnum = revnum * 10 + remnum;
+            x /= 10;
         }
 
-        if(revNum == realNum){
+        if (revnum == realnum) {
             return true;
         }
 
