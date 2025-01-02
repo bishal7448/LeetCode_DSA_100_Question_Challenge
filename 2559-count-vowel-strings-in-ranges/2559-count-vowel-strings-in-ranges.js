d576a94b2d738847699a3e5808b996bb7bbfd44b
Bishal Saha <saha.bishal7676@gmail.com>
2025-01-02 10:47:58 +0530

    Time: 6 ms (87.76%), Space: 78.1 MB (57.14%) - LeetHub

---
+++ 2559-count-vowel-strings-in-ranges/2559-count-vowel-strings-in-ranges.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string[]} words
+ * @param {number[][]} queries
+ * @return {number[]}
+ */
+var vowelStrings = function (words, queries) {
+    let prefix = [];
+    let res = [];
+    let count = 0;
+
+    function isVowel(c) {
+        return 'aeiou'.includes(c);
+    }
+
+    for (let word of words) {
+        if (isVowel(word[0]) && isVowel(word[word.length - 1])) {
+            count++;
+        }
+        prefix.push(count);
+    }
+
+    for (let query of queries) {
+        let [l, r] = query;
+        if (l === 0) {
+            res.push(prefix[r]);
+        } else {
+            res.push(prefix[r] - prefix[l - 1]);
+        }
+    }
+
+    return res;
+};
