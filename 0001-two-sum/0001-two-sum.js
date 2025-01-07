04fca339b5ef47261a2a37384039c93b7fd82341
Bishal Saha <saha.bishal7676@gmail.com>
2025-01-07 22:26:08 +0530

    Time: 50 ms (27.08%), Space: 49.6 MB (76.68%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -7,7 +7,7 @@ var twoSum = function (nums, target) {
 
     let ans = [];
 
-    for (let i = 0; i < nums.length; i++) {
+    for (let i = 0; i < nums.length - 1; i++) {
         for (let j = i+1; j < nums.length; j++) {
             if (nums[i] + nums[j] === target) {
 
