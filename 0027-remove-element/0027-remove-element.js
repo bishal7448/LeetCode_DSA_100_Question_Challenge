d9b46ae48c91c1cf923ae687812b87ec4ac40874
Bishal Saha <saha.bishal7676@gmail.com>
2025-01-09 10:53:22 +0530

    Time: 0 ms (100%), Space: 49.1 MB (52.84%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function (nums, val) {
+    let p = 0; //Pointer
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] != val) {
+            nums[p] = nums[i];
+            p++;
+        }
+    }
+
+    return p;
+};
