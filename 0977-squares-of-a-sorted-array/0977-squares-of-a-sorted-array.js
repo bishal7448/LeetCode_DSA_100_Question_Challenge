34052a60fb0b8d1a5f7134d86cf9ff98c6206ba0
Bishal Saha <saha.bishal7676@gmail.com>
2025-01-13 19:42:13 +0530

    Time: 1 ms (99.68%), Space: 56.4 MB (69.16%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
@@ -4,25 +4,22 @@
  */
 var sortedSquares = function (nums) {
     let ans = [];
-    ans.length = nums.length;
+    ans.length = nums.length
 
     let p1 = 0;
     let p2 = nums.length - 1;
-    let p3 = ans.length - 1;
 
-    while (p3 > -1) {
+    for (let i = nums.length - 1; i >= 0; i--) {
         let val1 = nums[p1] * nums[p1];
         let val2 = nums[p2] * nums[p2];
 
-        if (val1 > val2) {
-            ans[p3] = val1;
-            p1++;
-        } else {
-            ans[p3] = val2;
+        if (val1 < val2) {
+            ans[i] = val2;
             p2--;
+        } else {
+            ans[i] = val1;
+            p1++;
         }
-
-        p3--;
     }
 
     return ans;
