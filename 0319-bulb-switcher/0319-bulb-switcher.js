93bc4ff3466b69ad7bf5841b68e1db53aea41351
Bishal Saha <saha.bishal7676@gmail.com>
2025-01-15 20:29:34 +0530

    Time: 0 ms (100%), Space: 49.2 MB (17.48%) - LeetHub

--- 0319-bulb-switcher/0319-bulb-switcher.js
+++ 0319-bulb-switcher/0319-bulb-switcher.js
@@ -3,8 +3,8 @@
  * @return {number}
  */
 var bulbSwitch = function (n) {
-    let count = 0;
-    let i = 1;
+    let count = 0; //Number of bulbs that are on after n rounds
+    let i = 1; //initial number
 
     while (i * i <= n) {
         count++;
