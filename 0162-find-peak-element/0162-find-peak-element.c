558051cfd17fde14a7daf6ec20d052b0667858e5
Bishal Saha <saha.bishal7676@gmail.com>
2025-01-21 18:52:14 +0530

    Time: 0 ms (100%), Space: 8.3 MB (8.43%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.c
@@ -0,0 +1,34 @@
+int findPeakElement(int* nums, int numsSize) {
+    int len = numsSize;
+
+    if (len == 1) {
+
+        return 0;
+    } else if (nums[0] > nums[1]) {
+
+        return 0;
+    } else if (nums[len - 1] > nums[len - 2]) {
+
+        return len - 1;
+    } else {
+        int start = 1;
+        int end = len - 2;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
+
+                return mid;
+            } else if (nums[mid] < nums[mid + 1]) {
+                
+                start = mid + 1;
+            } else {
+
+                end = mid - 1;
+            }
+        }
+
+    return - 1;
+    }
+}
