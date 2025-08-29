02819eea5574539da53eda87a4cda1c7e4d53184
Bishal Saha <saha.bishal7676@gmail.com>
2025-08-29 14:42:21 +0530

    Time: 0 ms (100%), Space: 41.9 MB (47.32%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -5,22 +5,22 @@ class Solution {
         if(digits[n - 1] != 9) {
             digits[n - 1] = digits[n - 1] + 1;
             return digits;
-        }else {
-            digits[n - 1] = 0;
+        }
 
-            for(int i = n - 2; i >= 0; i--) {
-                if(digits[i] != 9) {
-                    digits[i] = digits[i] + 1;
-                    return digits;
-                }
-
+        digits[n - 1] = 0;
+        for(int i = n - 2; i >= 0; i--) {
+            if(digits[i] != 9) {
+                digits[i] = digits[i] + 1;
+                return digits;
+            }else {
                 digits[i] = 0;
             }
         }
 
-        int[] ans = new int[n + 1];
-        ans[0] = 1;
+        int[] newDigits = new int[n + 1];
 
-        return ans;       
+        newDigits[0] = 1;
+
+        return newDigits;
     }
 }
