11c323775b85842c938c457ac2b01fa9d924cab3
Bishal Saha <saha.bishal7676@gmail.com>
2025-10-17 12:21:23 +0530

    Time: 0 ms (100%), Space: 40.9 MB (6.27%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,23 +1,21 @@
 class Solution {
     public int fib(int n) {
-        if (n == 0) {
-
+        if(n == 0) {
             return 0;
-        } else if (n == 1) {
-
+        }else if(n == 1) {
             return 1;
+        }else {
+            int fst = 0;
+            int sndt = 1;
+            int trdt = 0;
+
+            for(int i = 1; i < n; i++) {
+                trdt = fst + sndt;
+                fst = sndt;
+                sndt = trdt;
+            }
+
+            return trdt;
         }
-
-        int ft = 0;
-        int st = 1;
-
-        for (int i = 1; i < n; i++) {
-            int tt = ft + st;
-
-            ft = st;
-            st = tt;
-        }
-
-        return st;
     }
 }
